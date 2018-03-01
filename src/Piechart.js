afc9a305c3a08614881cfb5117aed4237aa7e8d0
Swizec Teller <swizec@swizec.com>
2018-03-01 01:07:33 -0800

    Add hover effects

--- src/Piechart.js
+++ src/Piechart.js
@@ -4,9 +4,68 @@ import * as d3 from "d3";
 import * as chroma from "chroma-js";
 import styled from "styled-components";
 
-const Arc = styled.path`
+const SArc = styled.path`
     cursor: pointer;
 `;
+
+class Arc extends Component {
+    arc = d3
+        .arc()
+        .innerRadius(80)
+        .outerRadius(150)
+        .cornerRadius(8);
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            color: props.color,
+            origCol: props.color
+        };
+    }
+
+    componentWillReceiveProps(newProps) {
+        this.setState({ color: newProps.color });
+    }
+
+    click = () => {
+        const { data: { tag, amount } } = this.props.d;
+        console.log(
+            `$${Math.round(amount, 2)} for ${tag} in the last 12 months`
+        );
+    };
+
+    hover = () => {
+        this.setState({
+            color: this.state.color.saturate(2)
+        });
+    };
+
+    unhover = () => {
+        this.setState({
+            color: this.state.origCol
+        });
+    };
+
+    render() {
+        const { d } = this.props;
+        const { color } = this.state;
+
+        console.log(color);
+
+        return (
+            <SArc
+                d={this.arc(d)}
+                style={{
+                    fill: color
+                }}
+                onClick={this.click}
+                onMouseOver={this.hover}
+                onMouseOut={this.unhover}
+            />
+        );
+    }
+}
 
 class Piechart extends Component {
     pie = d3
@@ -14,16 +73,7 @@ class Piechart extends Component {
         .value(d => d.amount)
         .sortValues(d => d.tag)
         .padAngle(0.005);
-    arc = d3
-        .arc()
-        .innerRadius(80)
-        .outerRadius(150)
-        .cornerRadius(8);
     color = chroma.scale("PuBu");
-
-    clickArc({ data: { tag, amount } }) {
-        console.log(`$${amount} for ${tag} in the last 12 months`);
-    }
 
     render() {
         const { data, groupBy, x, y } = this.props;
@@ -41,11 +91,9 @@ class Piechart extends Component {
                 {this.pie(_data).map((d, i) => (
                     <g>
                         <Arc
-                            d={this.arc(d)}
-                            style={{
-                                fill: this.color(i / Object.keys(_data).length)
-                            }}
-                            onClick={() => this.clickArc(d)}
+                            d={d}
+                            color={this.color(i / Object.keys(_data).length)}
+                            key={d.data.tag}
                         />
                     </g>
                 ))}
